6e6698933b7ce28df7f7291af459aa5cec4a2740
sebastianSchwenke <96216134+0308612@users.noreply.github.com>
2025-04-29 13:23:04 +1000

    added soil moisture sensor
    attached soil moisture sensor. problems: not working with float due to float using decimal points when the soil moisture sensor not using decimal points so used long instead

--- robotics_task_3_assesment.ino
+++ robotics_task_3_assesment.ino
@@ -3,27 +3,40 @@
 #include <DHT_U.h>
 #include <SD.h>
 
-#define DHTTYPE DHT11;
+//#define DHTTYPE DHT11;
 
 DFRobot_DS1307 DS1307;
 
-DHT dht(DHTPIN, DHTTYPE);
+//DHT dht(DHTPIN, DHTTYPE);
+
+const int soilPin = A0;
+
+int debug = 1;
 
 void setup() {
+    pinMode(soilPin, INPUT);
+
     Serial.begin(115200);
     Serial.begin(9600);
 }
 
 void loop() {
-    Debugging();
+  long soilM = Soil();
+
+  long mostureValue = map(soilM, 0 ,1023, 0, 100);
+
+  if (debug){
+    Serial.print(mostureValue);
+    Serial.println("% moisture");
+
+    delay(200);
+  }
 }
 
-void Debugging() {
-  
-}
 
 long Soil(){
-  
+  long value = analogRead(soilPin);
+  return value;
 }
 
 void DCPump() {
